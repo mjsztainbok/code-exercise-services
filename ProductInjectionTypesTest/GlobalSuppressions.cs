72e81aa65dbd2a14881e2ae577a7e9d83a76a94c
Mark Sztainbok <mark@sztainbok.com>
2020-12-15 16:51:57 -0800

    Fix StyleCop warning in test projects

--- ProductInjectionTypesTest/GlobalSuppressions.cs
+++ ProductInjectionTypesTest/GlobalSuppressions.cs
@@ -5,5 +5,6 @@
 
 using System.Diagnostics.CodeAnalysis;
 
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA0001:XML comment analysis is disabled due to project configuration", Justification = "Test files")]
 [assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:File should have header", Justification = "Test files")]
 [assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "Test files")]
